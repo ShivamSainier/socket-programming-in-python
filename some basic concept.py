ff2ea076705b98510401d015f50f885298ff35e3
unknown <ss1907364@gmail.com>
2020-01-30 00:08:39 +0530

    some basic concepts of networking-socket

--- some basic concept.py
+++ some basic concept.py
@@ -1,4 +1,17 @@
 # ip address (internet protocol address) : ip address is a unique string of number saparated by fullstopthat identifies each computer using the internet
 # protocol to communicate over a network. every computer ,device,server have a unique ip address. ip address is static as well as dynamic type.
 # public ip addess: it can be find by the google by searching "whats my ip".
-# private addrss:open cmd and type "ipconfig" 
+# private addrss:open cmd and type "ipconfig"
+
+# sockets
+#  A socket is one endpoint two-way communication link between two programs running on the network. A spcket is bound to a port number so that the tcplayer can identify the application
+# that data is designed to send to . An endpoint is a combination of  an ip address and a port number.
+
+# sockets commands in python
+
+#socket.socket   #to create a socket
+#   s.bind(host/ip address,port)
+#s.send()             #to send the data or msg
+#s.listen()            #to understand the msg
+#s. recv()            #to recieve the msg
+#s.close()
