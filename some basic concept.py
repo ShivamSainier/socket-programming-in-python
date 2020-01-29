2cf3567f2632c6b770d74802663d7c509d8594a2
unknown <ss1907364@gmail.com>
2020-01-29 23:53:30 +0530

    some basic concepts of networking

---
+++ some basic concept.py
@@ -0,0 +1,4 @@
+# ip address (internet protocol address) : ip address is a unique string of number saparated by fullstopthat identifies each computer using the internet
+# protocol to communicate over a network. every computer ,device,server have a unique ip address. ip address is static as well as dynamic type.
+# public ip addess: it can be find by the google by searching "whats my ip".
+# private addrss:open cmd and type "ipconfig" 
