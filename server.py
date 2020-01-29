7941f10bd83af59ee93d88cf236b16f34e761038
unknown <ss1907364@gmail.com>
2020-01-30 01:30:30 +0530

    add a new socket binding function

--- server.py
+++ server.py
@@ -2,10 +2,33 @@ import socket
 import sys
 
 # create a socket function
-try:
-    host=""
-    port=999
-    s=socket.socket()   # to create a socket
-except socket.error as e:
-    print(f"socket creation error {e}")
+def socket():
+    try:
+        global host
+        global port
+        global s
+        host=""
+        port=999
+        s=socket.socket()   # to create a socket
+    except socket.error as e:
+        print(f"socket creation error {e}")
+
+ # create a bind function
+def bind():
+    try:
+        global host
+        global port
+        global s
+        print(f"binding the port {str(port)}")
+        s.bind(host/port)
+        s.listen(5)
+    except socket.error as e:
+        print(f"socket faild {e}")
+        bind()
+
+
+
+
+
+
 
