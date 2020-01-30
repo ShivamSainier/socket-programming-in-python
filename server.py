e5f0b687c1f066c5f2d86b6366a1004f53997d7f
unknown <ss1907364@gmail.com>
2020-01-31 00:53:25 +0530

    added file

--- server.py
+++ server.py
@@ -1,41 +1,12 @@
 import socket
-import sys
 
-# create a socket function
-def socket():
-    try:
-        global host
-        global port
-        global s
-        host=""
-        port=999
-        s=socket.socket()   # to create a socket
-    except socket.error as e:
-        print(f"socket creation error {e}")
-
- # create a bind function
-def bind():
-    try:
-        global host
-        global port
-        global s
-        print(f"binding the port {str(port)}")
-        s.bind(host/port)
-        s.listen(5)
-    except socket.error as e:
-        print(f"socket faild {e}")
-        bind()
-
- # Establish a connection with aa client
-
- def socket_accept():
-     con,address=s.accept()
-     print(f"conection has been establish {address[0]} and {address[1]}")
-     send_command(conn)
-     conn.close()
-
-
-
-
-
-
+s=socket.socket()
+print("socket genrate successfully")
+s.bind(("localhost",999))
+s.listen(3)
+while True:
+    c,add=s.accept()
+    name=c.recv(1024).decode()
+    print(f"connected with address {add}",name)
+    c.send(bytes("welcome to my home","utf-8"))
+    c.close()
