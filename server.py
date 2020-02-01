67c92ba83b9ef963546eddf1e8c2bd9458d77c7a
unknown <ss1907364@gmail.com>
2020-02-02 00:17:07 +0530

    add buffering

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 
+
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((socket.gethostname(),1234))
 s.listen(5)
@@ -7,5 +8,6 @@ while True:
     conn, address=s.accept()
     print(f"connection with {address} is establish")
     msg="welcome to the house"
-    conn.send(bytes(msg,"utf-8"))
+    full_msg=(f"{len(msg}:<10")
+    conn.send(bytes(full_msg,"utf-8"))
     conn.close()
